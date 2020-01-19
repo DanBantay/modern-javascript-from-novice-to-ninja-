0f85ff0f2469b7f386f888023597d1ebde1c7665
Dan Bantay <dbantayjr@gmail.com>
2020-01-19 11:29:40 -0800

    file cleanup and organization

---
+++ udemy.js
@@ -0,0 +1,173 @@
+/* eslint-disable */
+// // strings
+// console.log('it works!');
+
+// const email = 'dbantayjr@gmail.com';
+// console.log(email);
+
+// // string concatenation
+// const firstName = 'Dan';
+// const lastName = 'Bantay';
+
+// const fullName = `${firstName} ${lastName}`;
+// console.log(fullName);
+
+// // getting characters
+// console.log(fullName[2]);
+
+// // string length
+// console.log(fullName.length);
+
+// // sting methods
+// // add .() to methods
+// console.log(fullName.toLocaleUpperCase());
+// const result = fullName.toLocaleLowerCase();
+// console.log(result, fullName);
+
+// // indexOf is a method
+// // @ is an arguement
+// const index = email.indexOf('@');
+// console.log(index);
+
+// common string methods
+
+// const email = 'dbantayjr@gmail.com';
+
+// // const result = email.lastIndexOf('l');
+
+// // const result = email.slice(2, 5);
+
+// // let result = email.replace('n', 'r');
+
+// console.log(result);
+
+// const radius = 10;
+// const pi = 3.14;
+
+// console.log(radius, pi);
+
+// math operators +, -, *, /, **, %
+
+// console.log(10 / 2);
+// let result = radius % 3;
+
+// Order of operations PEMDAS
+
+// let result = 5 * (10 * 3)**2;
+
+// console.log(result);
+
+// // likes = likes + 1;
+// likes += 1;
+// // likes++;
+
+// console.log(likes);
+
+// NaN not a number
+// console.log(5 / 'hello');
+
+// const likes = 10;
+
+// // concatenation
+// const result = `the blog has ${likes} likes`;
+// console.log(result);
+
+// template strings or template literal
+
+const title = 'Best reads of 2019';
+const author = 'Stephen King';
+const likes = 1000;
+
+// concatenation way
+// const result =
+//   'The blog called ' + title + ' by ' + author + ' has ' + likes + ' likes';
+
+//   console.log(result);
+
+// // template string way (use back ticks for this)
+// const result = `The blog called ${title} by ${author} has ${likes} likes`;
+// console.log(result);
+
+// creating html templates
+// let html = `
+//     <h2>${title}</h2>
+//     <p>By ${author}</p>
+//     <span>This blog has ${likes} likes</span>
+//     `;
+// console.log(html);
+
+// let ninjas = ['ryu', 'akira', 'ken'];
+
+// ninjas[1] = 'ken';
+// console.log(ninjas[1]);
+
+// let ages = [20,25,30,35];
+// console.log(ages[2]);
+
+// let random = ['shaun', 'crystal', 30, 31];
+// console.log(random);
+
+// console.log(ninjas.length);
+
+// array methods
+
+// let result = ninjas.join('');
+// let result = ninjas.indexOf('akira'); finds out what place in index in array
+// join two things together
+// let result = ninjas.concat(['ken', 'crystal']); 
+// let result = ninjas.push('ken'); 
+// result = ninjas.pop()
+// console.log(result);
+
+// let age = null;
+
+// console.log(age, age, + 3, `the age is ${age}`);
+
+// booleans & comparisons
+// console.log(true, false, 'true', 'false');
+
+// methods can return booleans
+let email = 'dbantayjr@gmail.com';
+let names = ['mario', 'luigi', 'toad'];
+// let result = email.includes('@');
+// let result = names.includes('bowser');
+
+// console.log(result);
+
+// comparison operators
+// let age = 25;
+// console.log(age == 25);
+// console.log(age == 30);
+// // not equal
+// console.log(age != 30);
+// console.log(age > 20);
+// // less than or equal
+// console.log(age <= 25);
+
+// let name = 'shaun';
+
+// console.log(name == 'shaun');
+// console.log(name == 'Shaun');
+// console.log(name > 'crystal');
+// console.log(name > 'Shaun');
+// console.log(name > 'Crystal');
+
+// Type conversion
+
+let score = '100';
+
+score = Number(score);
+
+// console.log(score + 1);
+// console.log(typeof score);
+
+// let result = Number('hello');
+
+let result = String(50);
+
+console.log(result, typeof result);
+
+
+
+
+
