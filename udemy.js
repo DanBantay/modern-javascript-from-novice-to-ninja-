cb48514fb340aa62d81465fa3989f2fd46be2192
Dan Bantay <dbantayjr@gmail.com>
2020-03-12 12:26:41 -0700

    Map method

--- udemy.js
+++ udemy.js
@@ -257,28 +257,28 @@
 //     }
 // }
 // Switch statements
-const grade = 'C';
+// const grade = 'C';
 
-switch(grade) {
-    case 'A':
-        console.log('you got an A!');
-        break;
-    case 'B':
-        console.log('you got an B!');
-        break;
-    case 'C':
-        console.log('you got an C!');
-        break;
-    case 'D':
-        console.log('you got an D!');
-        break;
-    case 'F':
-        console.log('you got an F!');
-        break;
-        default:
-        console.log('not a valid grade');
+// switch(grade) {
+//     case 'A':
+//         console.log('you got an A!');
+//         break;
+//     case 'B':
+//         console.log('you got an B!');
+//         break;
+//     case 'C':
+//         console.log('you got an C!');
+//         break;
+//     case 'D':
+//         console.log('you got an D!');
+//         break;
+//     case 'F':
+//         console.log('you got an F!');
+//         break;
+//         default:
+//         console.log('not a valid grade');
        
-}
+// }
 
 //using if statments
 
