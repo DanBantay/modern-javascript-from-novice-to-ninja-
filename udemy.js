954cb922647dbcb9a9d0067f6bffbb7f5ffb07c0
Dan Bantay <dbantayjr@gmail.com>
2020-03-11 17:55:07 -0700

    switch statements

--- udemy.js
+++ udemy.js
@@ -242,13 +242,43 @@
 // console.log(!false);
 
 //Break and continue
-const scores = [50, 25, 0, 30, 100, 20 , 10];
+// const scores = [50, 25, 0, 30, 100, 20 , 10];
 
-for(let i = 0; i < scores.length; i++) {
-    console.log('your score: ', scores[i]);
+// for(let i = 0; i < scores.length; i++) {
+    
+//     if(scores[i] === 0){
+//         continue;
+//     }
+//     console.log('your score: ', scores[i]);
 
-    if(scores[i] === 100) {
-        console.log('congrats you got the top score');  
+//     if(scores[i] === 100) {
+//         console.log('congrats you got the top score');  
+//         break;
+//     }
+// }
+// Switch statements
+const grade = 'C';
+
+switch(grade) {
+    case 'A':
+        console.log('you got an A!');
         break;
-    }
+    case 'B':
+        console.log('you got an B!');
+        break;
+    case 'C':
+        console.log('you got an C!');
+        break;
+    case 'D':
+        console.log('you got an D!');
+        break;
+    case 'F':
+        console.log('you got an F!');
+        break;
+        default:
+        console.log('not a valid grade');
+       
 }
+
+//using if statments
+
