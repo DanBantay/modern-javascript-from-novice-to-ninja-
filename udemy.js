8a47d686eb597debd54c3fd28cc7fd972018817d
Dan Bantay <dbantayjr@gmail.com>
2020-03-12 12:46:29 -0700

    Map method

--- udemy.js
+++ udemy.js
@@ -280,5 +280,27 @@
        
 // }
 
-//using if statments
+// Map method
 
+const prices = [20, 20, 30, 25, 15, 40, 80, 5];
+
+// const salePrices = prices.map((price) => price / 2);
+
+// console.log(salePrices); 
+
+const products = [
+    {name: 'gold star', price: 20},
+    {name: 'mushroom', price: 40},
+    {name: 'green shells', price: 30},
+    {name: 'banana skin', price: 10},
+    {name: 'red shells', price: 50},
+];
+
+const saleProduct = products.map((product) => {
+    if (product.price > 30){
+        return{name: product.name, price: product.price / 2};
+    } else {
+        return product;
+    }
+});
+console.log(saleProduct, products);
